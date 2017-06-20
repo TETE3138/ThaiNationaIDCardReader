3d464ff1fe696462a54aa15166a031e250e11461
tewich tantigate <tewichtete31@hotmail.com>
2017-06-20 12:26:00 +0700

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using ThaiNationalIDCard;
+using System.Runtime.InteropServices;
+using Windows.Devices.SmartCards;
+using Windows.Devices.Enumeration;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+using System.Drawing;
+using Windows.Storage.Streams;
+using Windows.Storage;
+using Windows.ApplicationModel;
+using PCSC;
+using Windows.UI.Core;
+using System.Data.Common;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ThaiNationalIDCardReader
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        ThaiIDCard idcard;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Button_Click(null, null);
+            TogglePicture.IsOn = true;
+
+   
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Debug.WriteLine("Reading...");
+
+                Personal personal = idcard.readAll(true);
+                if (personal != null)
+                {
+                    Debug.WriteLine(personal.Citizenid);
+                    Debug.WriteLine(personal.Birthday.ToString("dd/MM/yyyy"));
+                    Debug.WriteLine(personal.Sex);
+                    Debug.WriteLine(personal.Th_Prefix);
+                    Debug.WriteLine(personal.Th_Firstname);
+                    Debug.WriteLine(personal.Th_Lastname);
+                    Debug.WriteLine(personal.En_Prefix);
+
+                    Debug.WriteLine(personal.En_Firstname);
+                    Debug.WriteLine(personal.En_Lastname);
+                    Debug.WriteLine(personal.Issue.ToString("dd/MM/yyyy"));
+                    Debug.WriteLine(personal.Expire.ToString("dd/MM/yyyy"));
+                    /*
+                    LogLine(personal.Address);
+                    LogLine(personal.addrHouseNo); // บ้านเลขที่ 
+                    LogLine(personal.addrVillageNo); // หมู่ที่
+                    LogLine(personal.addrLane); // ซอย
+                    LogLine(personal.addrRoad); // ถนน
+                    LogLine(personal.addrTambol);
+                    LogLine(personal.addrAmphur);
+                    LogLine(personal.addrProvince);*/
+                }
+                else if (idcard.ErrorCode() > 0)
+                {
+                    Debug.WriteLine(idcard.Error());
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        async void cardInserted(Personal p)
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+             async () =>
+                     {
+                         //ui update
+                         txtIdCitiz.Text = p.Citizenid;
+                         txtThaiName.Text = String.Format("{0} {1} {2}", p.Th_Prefix, p.Th_Firstname, p.Th_Lastname);
+                         txtEngName.Text = String.Format("{0} {1} {2}", p.En_Prefix, p.En_Firstname, p.En_Lastname);
+                         txtBirthDate.Text = p.Birthday.ToString("dd MMMM yyyy");
+                         txtGender.Text = p.Sex.Trim() == "1" ? "ชาย" : "หญิง";
+                         txtAddress1.Text = String.Format("{0} {1} {2} {3}", p.addrHouseNo, p.addrVillageNo, p.addrLane, p.addrRoad);
+                         txtAddress2.Text = String.Format("{0} {1} {2}", p.addrTambol, p.addrAmphur, p.addrProvince);
+
+                         txtIssueDate.Text = p.Issue.ToString("d");
+                         txtExpireDate.Text = p.Expire.ToString("d");
+                         txtpostcode.Text = await FindPostalcodeByTambon(p.addrTambol);
+                         if (TogglePicture.IsOn)
+                             Image1.Source = await p.DecodePhoto();
+                     }
+                    );
+        }
+
+        private void TogglePicture_Toggled(object sender, RoutedEventArgs e)
+        {
+            if (TogglePicture.IsOn == true)
+            {
+                idcard.eventCardInserted -= cardInserted;
+                idcard.eventCardInsertedWithPhoto -= cardInserted;
+                idcard.eventCardInsertedWithPhoto += cardInserted;
+            }
+            else
+            {
+                Image1.Source = null;
+                idcard.eventCardInsertedWithPhoto -= cardInserted;
+                idcard.eventCardInserted += cardInserted;
+            }
+        }
+
+        //refresh button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (idcard != null)
+                idcard.Dispose();
+            idcard = new ThaiIDCard();
+
+            try
+            {
+                if (idcard.GetReaders().Length >= 1)
+                {
+                    idcard.MonitorStart(idcard.GetReaders()[0]);
+                    txtCardReaderName.Text = idcard.GetReaders()[0];
+                    TogglePicture_Toggled(null, null);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                txtCardReaderName.Text = String.Format("({0})", ex.Message);
+            }
+        }
+
+
+
+
+        private async Task<String> FindPostalcodeByTambon(String Tambon)
+        {
+            if (Tambon.StartsWith("เขต"))
+                Tambon = Tambon.Substring(3, Tambon.Length - 3);
+            if (Tambon.StartsWith("แขวง"))
+                Tambon = Tambon.Substring(4, Tambon.Length - 4);
+
+            string fname = @"Assets\tambon-2.csv";
+
+            StorageFolder InstallationFolder = Package.Current.InstalledLocation;
+            StorageFile file;
+            try
+            {
+                file = await InstallationFolder.GetFileAsync(fname);
+            }
+            catch (FileNotFoundException)
+            {
+                //TODO: Handler FileNotFoundException              
+                throw new FileNotFoundException(String.Format(@"Couldn't find '{0}'", fname));
+            }
+
+            String[] lines = File.ReadAllLines(file.Path);
+            //return Array.Find<String>(lines, (line) => line.Split(",".ToCharArray()[0])[0].Trim() == Tambon);       
+            foreach (String line in lines)
+            {
+                String[] s = line.Split(",".ToCharArray()[0]);
+                if (s[0] == Tambon)
+                    return s[1];
+            }
+            return String.Empty;
+
+        }
+
+    }
+}
---
+++ ThaiIDCard.cs
@@ -0,0 +1,448 @@
+﻿/* BSD license
+ * Credit:  APDU Command from Mr.Manoi http://hosxp.net/index.php?option=com_smf&topic=22496
+ * Require add reference: PresentationCore, System.Xaml, WindowsBase
+ * Require add refrernce(Nuget): PCSC ( http://www.nuget.org/packages/PCSC/ )
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using PCSC;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ThaiNationalIDCard
+{
+    public delegate void handlePhotoProgress(int value, int maximum);
+    public delegate void handleCardInserted(Personal personal);
+    public delegate void handleCardRemoved();
+
+    public class ThaiIDCard : IDisposable
+    {
+        const int ECODE_SCardError = 256;
+        const int ECODE_UNSUPPORT_CARD = 1;
+
+        private static readonly IContextFactory _contextFactory = ContextFactory.Instance;
+        private ISCardContext _hContext;
+        private SCardReader _reader;
+        private SCardError _err;
+        private IntPtr _pioSendPci;
+
+        private IAPDU_THAILAND_IDCARD _apdu;
+        private SCardMonitor _monitor;
+
+        private string _error_message;
+        private int _error_code;
+
+        public event handlePhotoProgress eventPhotoProgress;
+        public event handleCardInserted eventCardInserted;
+        public event handleCardInserted eventCardInsertedWithPhoto;
+        public event handleCardRemoved eventCardRemoved;
+
+        public int ErrorCode()
+        {
+            return _error_code;
+        }
+
+        public string Error()
+        {
+            return _error_message;
+        }
+
+        public string Version()
+        {
+            throw new NotImplementedException();
+
+            // return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        }
+
+        private void _Inserted(CardStatusEventArgs args)
+        {
+            try
+            {
+                Personal personal;
+                if (eventCardInserted != null)
+                {
+                    personal = readAll(false, args.ReaderName);
+                    eventCardInserted(personal);
+                }
+                if (eventCardInsertedWithPhoto != null)
+                {
+                    personal = readAll(true, args.ReaderName);
+                    eventCardInsertedWithPhoto(personal);
+                }
+            }
+            catch (PCSCException ex)
+            {
+                _error_code = ECODE_SCardError;
+                _error_message = "_Inserted Err: " + ex.Message + " (" + ex.SCardError.ToString() + ")";
+                Debug.WriteLine(_error_message);
+            }
+
+        }
+
+        private void _Removed(string eventName, CardStatusEventArgs unknown)
+        {
+            if (eventCardRemoved != null)
+            {
+                eventCardRemoved();
+            }
+        }
+
+        public bool MonitorStart(string readerName)
+        {
+            try
+            {
+                _monitor = new SCardMonitor(_contextFactory, SCardScope.System);
+                _monitor.CardInserted += (sender, args) => _Inserted(args);
+                _monitor.CardRemoved += (sender, args) => _Removed("CardRemoved", args);
+
+                _monitor.Start(readerName);
+                return true;
+            }
+            catch (PCSCException ex)
+            {
+                _error_code = ECODE_SCardError;
+                _error_message = "MonitorStart Err: " + ex.Message + " (" + ex.SCardError.ToString() + ")";
+                Debug.WriteLine(_error_message);
+                return false;
+            }
+        }
+        public bool MonitorStop(string readerName)
+        {
+            try
+            {
+                if (_monitor != null)
+                    _monitor.Cancel();
+                return true;
+
+            }
+            catch (PCSCException ex)
+            {
+                _error_code = ECODE_SCardError;
+                _error_message = "MonitorStop Err: " + ex.Message + " (" + ex.SCardError.ToString() + ")";
+                Debug.WriteLine(_error_message);
+                return false;
+            }
+        }
+
+        private void CheckErr(SCardError _err)
+        {
+            if (_err != SCardError.Success)
+                throw new PCSCException(_err,
+                    SCardHelper.StringifyError(_err));
+        }
+
+        private string GetUTF8FromAsciiBytes(byte[] ascii_bytes)
+        {
+            byte[] utf8;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            utf8 = Encoding.Convert(
+                Encoding.GetEncoding("TIS-620"),
+                Encoding.UTF8,
+                ascii_bytes
+                );
+            string result = System.Text.Encoding.UTF8.GetString(utf8);
+            return result.Substring(0, result.Length - 2);
+        }
+
+        private byte[] SendCommand(byte[][] commands)
+        {
+            byte[] pbRecvBuffer;
+            pbRecvBuffer = new byte[256];
+            foreach (byte[] command in commands)
+            {
+                pbRecvBuffer = new byte[256];
+                _err = _reader.Transmit(_pioSendPci, command, ref pbRecvBuffer);
+                CheckErr(_err);
+            }
+            return pbRecvBuffer;
+        }
+
+        private byte[] SendPhotoCommand()
+        {
+            var s = new System.IO.MemoryStream();
+            CMD_PAIR[] cmds_photo = _apdu.GET_CMD_CARD_PHOTO();
+
+            for (int i = 0; i < cmds_photo.Length; i++)
+            {
+                byte[] recv1 = new byte[256];
+                _err = _reader.Transmit(_pioSendPci, cmds_photo[i].CMD1, ref recv1);
+                CheckErr(_err);
+
+                if (recv1.Length > 0)
+                {
+                    byte[] recv2 = new byte[256];
+                    _err = _reader.Transmit(_pioSendPci, cmds_photo[i].CMD2, ref recv2);
+                    CheckErr(_err);
+                    //s.Write(recv2, 0, xwd);
+                    s.Write(recv2, 0, recv2.Length - 2);
+                }
+                if (eventPhotoProgress != null)
+                    eventPhotoProgress(i + 1, cmds_photo.Length);
+            }
+
+            s.Seek(0, SeekOrigin.Begin);
+            return s.ToArray();
+        }
+
+
+        public string[] GetReaders()
+        {
+            try
+            {
+                _hContext = _contextFactory.Establish(SCardScope.System);
+                string[] szReaders = _hContext.GetReaders();
+                _hContext.Release();
+
+                if (szReaders.Length <= 0)
+                    throw new PCSCException(SCardError.NoReadersAvailable,
+                        "Could not find any Smartcard reader.");
+                return szReaders;
+            }
+            catch (PCSCException ex)
+            {
+                _error_code = ECODE_SCardError;
+                _error_message = "GetReaders Err: " + ex.Message + " (" + ex.SCardError.ToString() + ")";
+                Debug.WriteLine(_error_message);
+                throw ex;
+            }
+        }
+
+
+        public Boolean Open(string readerName = null)
+        {
+            try
+            {
+                // delay 1.5 second for ATR reading.
+                System.Threading.Tasks.Task.Delay(1500).Wait();
+
+                _hContext = _contextFactory.Establish(SCardScope.System);
+                _reader = new SCardReader(_hContext);
+
+                // Connect to the card
+                if (String.IsNullOrEmpty(readerName))
+                {
+                    // Open first avaliable reader.
+                    // Retrieve the list of Smartcard _readers
+                    string[] szReaders = _hContext.GetReaders();
+                    if (szReaders.Length <= 0)
+                        throw new PCSCException(SCardError.NoReadersAvailable,
+                            "Could not find any Smartcard _reader.");
+
+                    _err = _reader.Connect(szReaders[0],
+                                SCardShareMode.Exclusive,
+                                SCardProtocol.T0 | SCardProtocol.T1);
+                    CheckErr(_err);
+                }
+                else
+                {
+                    _err = _reader.Connect(readerName,
+                                SCardShareMode.Shared,
+                                SCardProtocol.T0 | SCardProtocol.T1);
+                    CheckErr(_err);
+                }
+
+
+                _pioSendPci = new IntPtr();
+                switch (_reader.ActiveProtocol)
+                {
+                    case SCardProtocol.T0:
+                        _pioSendPci = SCardPCI.T0;
+                        break;
+                    case SCardProtocol.T1:
+                        _pioSendPci = SCardPCI.T1;
+                        break;
+                    default:
+                        throw new PCSCException(SCardError.ProtocolMismatch,
+                            "Protocol not supported: "
+                            + _reader.ActiveProtocol.ToString());
+                }
+
+                string[] readerNames;
+                SCardProtocol proto;
+                SCardState state;
+                byte[] atr;
+
+                var sc = _reader.Status(
+                    out readerNames,    // contains the reader name(s)
+                    out state,          // contains the current state (flags)
+                    out proto,          // contains the currently used communication protocol
+                    out atr);           // contains the ATR
+
+                if (atr == null || atr.Length < 2)
+                {
+                    return false;
+                }
+
+                if (atr[0] == 0x3B && atr[1] == 0x68)       //Smart card tested with old type (Figure A.)
+                {
+                    _apdu = new APDU_THAILAND_IDCARD_TYPE_02();
+                }
+                else if (atr[0] == 0x3B && atr[1] == 0x78)   //Smart card tested with new type (figure B.) 
+                {
+                    _apdu = new APDU_THAILAND_IDCARD_TYPE_02();
+                }
+                else if (atr[0] == 0x3B && atr[1] == 0x79)   // add 2016-07-10
+                {
+                    _apdu = new APDU_THAILAND_IDCARD_TYPE_02();
+                }
+                else if (atr[0] == 0x3B && atr[1] == 0x67)
+                {
+                    _apdu = new APDU_THAILAND_IDCARD_TYPE_01();
+                }
+                else
+                {
+                    // try unlisted card with TYPE 02
+                    _apdu = new APDU_THAILAND_IDCARD_TYPE_02();
+
+                    // Check ID checksum
+                    SendCommand(_apdu.CMD_SELECT());
+                    if (Checksum(GetUTF8FromAsciiBytes(SendCommand(_apdu.CMD_CID()))))
+                    {
+                        return true;
+                    }
+
+                    _error_code = ECODE_UNSUPPORT_CARD;
+                    _error_message = "Card not support";
+                    Debug.WriteLine(_error_message);
+                    return false;
+                }
+
+
+                return true;
+            }
+            catch (PCSCException ex)
+            {
+                _error_code = ECODE_SCardError;
+                _error_message = "Open Err: " + ex.Message + " (" + ex.SCardError.ToString() + ")";
+                Debug.WriteLine(_error_message);
+                return false;
+            }
+        }
+
+        public Boolean Close()
+        {
+            try
+            {
+                _reader.Disconnect(SCardReaderDisposition.Leave);
+                _hContext.Release();
+                return true;
+            }
+            catch (PCSCException ex)
+            {
+                _error_code = ECODE_SCardError;
+                _error_message = "Close Err: " + ex.Message + " (" + ex.SCardError.ToString() + ")";
+                Debug.WriteLine(_error_message);
+                return false;
+            }
+        }
+
+        public Personal readCitizenid(string readerName = null)
+        {
+            Personal personal = new Personal();
+            if (Open(readerName))
+            {
+                // Send SELECT/RESET command
+                SendCommand(_apdu.CMD_SELECT());
+
+                // CID
+                personal.Citizenid = GetUTF8FromAsciiBytes(SendCommand(_apdu.CMD_CID()));
+
+                Close();
+                return personal;
+            }
+
+            return null;
+        }
+
+        public Personal readAll(bool with_photo = false, string readerName = null)
+        {
+            Personal personal = new Personal();
+            if (Open(readerName))
+            {
+                // Send SELECT/RESET command
+                SendCommand(_apdu.CMD_SELECT());
+
+                // CID
+                personal.Citizenid = GetUTF8FromAsciiBytes(SendCommand(_apdu.CMD_CID()));
+
+                // Fullname Thai + Eng + BirthDate + Sex
+                personal.Info = GetUTF8FromAsciiBytes(SendCommand(_apdu.CMD_PERSON_INFO()));
+
+                // Address
+                personal.Address = GetUTF8FromAsciiBytes(SendCommand(_apdu.CMD_ADDRESS()));
+
+                // issue/expire
+                personal.Issue_Expire = GetUTF8FromAsciiBytes(SendCommand(_apdu.CMD_CARD_ISSUE_EXPIRE()));
+
+                if (with_photo)
+                {
+                    // Photo
+                    personal.PhotoRaw = SendPhotoCommand();
+                }
+
+
+                Close();
+                return personal;
+            }
+            return null;
+        }
+
+        public Personal readAllPhoto(string readerName = null)
+        {
+            return readAll(true, readerName);
+        }
+
+        public bool Checksum(string Citizenid)
+        {
+            int sum = 0;
+
+            if (Citizenid.Length != 13) return false;
+
+            for (int i = 0; i < 12; i++)
+            {
+                sum += Int32.Parse(Citizenid.Substring(i, 1)) * (13 - i);
+            }
+
+            if (((11 - (sum % 11)) % 10) == Int32.Parse(Citizenid.Substring(12, 1)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (eventPhotoProgress != null)
+                foreach (Delegate d in eventPhotoProgress.GetInvocationList())
+                {
+                    eventPhotoProgress -= (handlePhotoProgress)d;
+                }
+            if (eventCardInserted != null)
+                foreach (Delegate d in eventCardInserted.GetInvocationList())
+                {
+                    eventCardInserted -= (handleCardInserted)d;
+                }
+            if (eventCardInsertedWithPhoto != null)
+                foreach (Delegate d in eventCardInsertedWithPhoto.GetInvocationList())
+                {
+                    eventCardInsertedWithPhoto -= (handleCardInserted)d;
+                }
+            if (eventCardRemoved != null)
+                foreach (Delegate d in eventCardRemoved.GetInvocationList())
+                {
+                    eventCardRemoved -= (handleCardRemoved)d;
+                }
+        }
+    } // End class
+
+
+}
